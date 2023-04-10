f8efe81422b91e78321f4536d1cc09a2d87cdfad
Cem <cem.denizsel@gmail.com>
2023-04-10 19:06:59 +0200

    Calculation of APY for each asset added.

--- aave.js
+++ aave.js
@@ -9,12 +9,27 @@ const web3 = new Web3("https://ethereum.publicnode.com");
 // HTTP PROVIDER FOR AVALANCHE MAIINET
 //const web3 = new Web3("https://rpc.ankr.com/avalanche");
 
+// aaave protocol v2 lending pool abi for ETHEREUM mainnet
+const lendingPoolABI = require('./aave-abi-lending-pool-v2.json'); // ABI for AAVE v2 Lending Pool
+// aaave protocol v2 lending pool abi for POLYGON mainnet
+//const lendingPoolABI = require('./aave-abi-lending-pool-v2-polygon-market.json'); // ABI for POLYGON AAVE v2 Lending Pool
+// aaave protocol v2 lending pool abi for AVALANCHE mainnet
+//const lendingPoolABI = require('./aave-abi-lending-pool-v2-avalanche-market.json'); // ABI for AVALANCHE AAVE v2 Lending Pool
+
+
 //aave protocol data provider abi for ETHEREUM mainnet
 const aaveABI = require('./aave-abi.json'); // ABI for AAVE v2 Protocol Data Provider
 // aave protocol data provider abi for POLYGON mainnet
 //const aaveABI = require('./aave-abi-polygon-market.json'); // ABI for POLYGON AAVE v2 Protocol Data Provider
 // aave protocol data provider abi for AVALANCHE mainnet
 //const aaveABI = require('./aave-abi-avalanche-market.json'); // ABI for AVALANCHE AAVE v2 Protocol Data Provider
+
+// aave protocol lending pool address for ETHEREUM mainnet
+const lendingPoolAddress = "0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9";
+// aave protocol lending pool address for POLYGON mainnet
+//const lendingPoolAddress = "0x8dFf5E27EA6b7AC08EbFdf9eB090F32ee9a30fcf";
+// aave protocol lending pool address for AVALANCHE mainnet
+//const lendingPoolAddress = "0x4F01AeD16D97E3aB5ab2B501154DC9bb0F1A5A2C";
 
 //aave protocol data provider address for ETHEREUM mainnet
 const aaveAddress = "0x057835Ad21a177dbdd3090bB1CAE03EaCF78Fc6d";
@@ -25,7 +40,9 @@ const aaveAddress = "0x057835Ad21a177dbdd3090bB1CAE03EaCF78Fc6d";
 
 
 //DAI address for ethereum mainnet
-const assetAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F"; // DAI
+const daiAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F"; // DAI
+// USDT address for ethereum mainnet
+const usdtAddress = "0xdAC17F958D2ee523a2206206994597C13D831ec7"; // USDT
 
 
 
@@ -35,6 +52,8 @@ const ChainlinkABI = require('./chainlink_oracle.json');
 
 
 const aaveContract = new web3.eth.Contract(aaveABI, aaveAddress);
+const LendingPool = new web3.eth.Contract(lendingPoolABI, lendingPoolAddress);
+
 const priceFeed = new web3.eth.Contract(ChainlinkABI, address_Chainlink_Price_Feed);
 
 
@@ -53,6 +72,78 @@ async function getTVL() {
   } catch (error) {
     console.log("Error: " + error);
   }  
+}
+
+async function getAPYofEachAsset() {
+  try {
+    const reservesData = await aaveContract.methods.getAllReservesTokens().call();
+    
+    const promises = reservesData.map(async (reserveData) => {
+    //console.log("reserve Data", reserveData);
+    const apyData =  await getAPYforOneAsset(reserveData.tokenAddress);
+    const [depositAPY, variableBorrowAPY, stableBorrowAPY] = apyData;
+    console.log("------------------------------------------------");
+    console.log(reserveData.symbol, depositAPY, variableBorrowAPY, stableBorrowAPY);  
+    console.log("------------------------------------------------");
+    const parsedData = {
+      symbol: reserveData.symbol,
+      depositAPY: depositAPY,
+      variableBorrowAPY: variableBorrowAPY,
+      stableBorrowAPY: stableBorrowAPY
+    };
+  
+    return parsedData;
+  
+  });
+    
+    const results = await Promise.all(promises);
+    console.log(results);
+    return results;
+    
+  } catch (error) {
+    console.log("Error: " + error);
+  }  
+}
+
+async function getAPYforOneAsset(_assetAddress) {
+  
+ try {
+  const reserveData = await LendingPool.methods.getReserveData(_assetAddress).call();
+  //console.log("Reserve Data",reserveData);
+  const liquidityIndex = reserveData.liquidityIndex;
+  const variableBorrowIndex = reserveData.variableBorrowIndex;
+  const currentLiquidityRate = reserveData.currentLiquidityRate;
+  const currentVariableBorrowRate = reserveData.currentVariableBorrowRate;
+  const currentStableBorrowRate = reserveData.currentStableBorrowRate;
+  // console.log("------------------------------------------------");
+  // console.log(currentLiquidityRate, currentVariableBorrowRate, currentStableBorrowRate);
+  // console.log("------------------------------------------------");
+  const aTokenAddress = reserveData.aTokenAddress;
+  const stableDebtTokenAddress = reserveData.stableDebtTokenAddress;
+  const variableDebtTokenAddress = reserveData.variableDebtTokenAddress;
+
+  const RAY = 10**27; // 10 to the power 27
+  const SECONDS_PER_YEAR = 31536000;
+
+// Deposit and Borrow calculations
+// APY and APR are returned here as decimals, multiply by 100 to get the percents
+
+const depositAPR = currentLiquidityRate/RAY;
+const variableBorrowAPR = currentVariableBorrowRate/RAY;
+const stableBorrowAPR = currentStableBorrowRate/RAY;
+
+const depositAPY = (((1 + (depositAPR / SECONDS_PER_YEAR)) ** SECONDS_PER_YEAR) - 1)*100;
+const variableBorrowAPY = (((1 + (variableBorrowAPR / SECONDS_PER_YEAR)) ** SECONDS_PER_YEAR) - 1)*100;
+const stableBorrowAPY = (((1 + (stableBorrowAPR / SECONDS_PER_YEAR)) ** SECONDS_PER_YEAR) - 1)*100;
+// console.log("Deposit APY: " + depositAPY);
+// console.log("Variable Borrow APY: " + variableBorrowAPY);
+// console.log("Stable Borrow APY: " + stableBorrowAPY);
+return [depositAPY, variableBorrowAPY, stableBorrowAPY];
+ 
+} catch (error) {
+  console.log("Error: " + error);
+ }
+  
 }
 
 
@@ -75,7 +166,7 @@ async function getTVL() {
 
   // We get the reserve data for each reserve 
   const reserveDataPromises = reserves.map(async reserve => {
-    const reserveData = await aaveContract.methods.getReserveData(reserve.tokenAddress).call();
+  const reserveData = await aaveContract.methods.getReserveData(reserve.tokenAddress).call();
 
     // Parse them to make it seem more readable
     const parsedData = {
@@ -118,7 +209,10 @@ async function getTotalBorrowSupply() {
       //getTotalBorrowSupply();
      //const reservesData = await aaveContract.methods.getAllReservesTokens().call();
      //console.log(reservesData);
-    getSupplyBorrowAssets();
+    //getSupplyBorrowAssets();
+    getAPYofEachAsset();
+      //  getAPYforOneAsset(usdtAddress);
+      //  getAPYforOneAsset(daiAddress);
 
      //console.log("Result new: " + (result+22));
     //const result = await getTVL();
